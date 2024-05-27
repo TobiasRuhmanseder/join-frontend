b3fb8d58aa82af526d7f2b382bf1337d4ca4f192
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2024-05-28 00:59:00 +0200

    first commit

---
+++ src/app/login/sign-up-dialog/sign-up-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up-dialog.component.html',
+  styleUrl: './sign-up-dialog.component.scss'
+})
+export class SignUpDialogComponent {
+
+}
